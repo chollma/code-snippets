2e760b4f1ba3573ea2538b7e0bb1895292f889ac
chollma <53924848+chollma@users.noreply.github.com>
2020-09-08 12:22:52 -0700

    Initial commit

---
+++ shell-example.py
@@ -0,0 +1,9 @@
+Python 3.7.9 (tags/v3.7.9:13c94747c7, Aug 17 2020, 18:01:55) [MSC v.1900 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license" for more information.
+>>> x = 4
+>>> print(x)
+4
+>>> x + 4
+8
+>>> print('hello world')
+hello world
